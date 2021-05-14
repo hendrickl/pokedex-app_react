373b74b6add9b6413f4fa2de780f8d52b78edc43
hendrick <h.lincertin@gmail.com>
2021-05-14 19:54:13 +0200

    feat: implement the navigation with react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,25 @@
 import React, { FunctionComponent} from 'react';
 import PokemonList from './pages/pokemon-list';
+import PokemonsDetail from './pages/pokemon-detail';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 
 const App: FunctionComponent = () => {
 
     return (
-        <PokemonList />
+        <Router>
+            <div>
+                <nav>
+                    <div className='nav-wrapper teal'>
+                        <Link to='/' className='brand-logo center'>Pokedex</Link>
+                    </div>
+                </nav>
+                <Switch>
+                    <Route exact path='/' component={PokemonList} />
+                    <Route exact path='/pokemons' component={PokemonList} />
+                    <Route path='/pokemons/:id' component={PokemonsDetail} />
+                </Switch>
+            </div>
+        </Router>
     )
 }
 
