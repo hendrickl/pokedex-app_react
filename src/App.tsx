516f2675add17420a4d8a04d0a5e65cd4159020a
hendrick <h.lincertin@gmail.com>
2021-05-15 14:49:20 +0200

    feat: implement page-not-found route

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React, { FunctionComponent} from 'react';
 import PokemonList from './pages/pokemon-list';
 import PokemonsDetail from './pages/pokemon-detail';
 import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import PageNotFound from './pages/page-not-found';
 
 const App: FunctionComponent = () => {
 
@@ -17,6 +18,7 @@ const App: FunctionComponent = () => {
                     <Route exact path='/' component={PokemonList} />
                     <Route exact path='/pokemons' component={PokemonList} />
                     <Route path='/pokemons/:id' component={PokemonsDetail} />
+                    <Route component={PageNotFound} />
                 </Switch>
             </div>
         </Router>
