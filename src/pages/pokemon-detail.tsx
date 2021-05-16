bc6d192dc277de2394060354be7a0c2eaee77704
hendrick <h.lincertin@gmail.com>
2021-05-16 18:08:28 +0200

    feat: implement fetch api

--- src/pages/pokemon-detail.tsx
+++ src/pages/pokemon-detail.tsx
@@ -1,7 +1,6 @@
 import React, { FunctionComponent, useState, useEffect } from 'react';
 import { RouteComponentProps, Link } from 'react-router-dom';
 import Pokemon from '../models/pokemon';
-import POKEMONS from '../models/mock-pokemon';
 import formatDate from '../helpers/format-date';
 import formatType from '../helpers/format-type';
   
@@ -12,12 +11,12 @@ const PokemonsDetail: FunctionComponent<RouteComponentProps<Params>> = ({ match 
   const [pokemon, setPokemon] = useState<Pokemon|null>(null);
   
   useEffect(() => {
-    POKEMONS.forEach(pokemon => {
-      if (match.params.id === pokemon.id.toString()) {
-        setPokemon(pokemon);
-      }
-    })
-  }, [match.params.id]);
+      fetch(`http://localhost:3001/pokemons/${match.params.id}`)
+      .then(response => response.json())
+      .then(pokemon => {
+        if(pokemon.id) setPokemon(pokemon)
+      });
+    }, [match.params.id]);
     
   return (
     <div>
--- src/pages/pokemon-list.tsx
+++ src/pages/pokemon-list.tsx
@@ -1,13 +1,16 @@
 import React, { FunctionComponent, useState, useEffect } from 'react';
 import Pokemon from '../models/pokemon';
-import POKEMONS from '../models/mock-pokemon';
 import PokemonCard from '../components/pokemon-card';
   
 const PokemonList: FunctionComponent = () => {
   const [pokemons, setPokemons] = useState<Pokemon[]>([]);
   
   useEffect(() => {
-    setPokemons(POKEMONS);
+    fetch('http://localhost:3001/pokemons')
+    .then(response => response.json())
+    .then((pokemons) => {
+      setPokemons(pokemons)
+    });
   }, []);
   
   return (
