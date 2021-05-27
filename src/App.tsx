dbe80e517da0e1318d43c63ff5c8439efc0b9264
hendrick <h.lincertin@gmail.com>
2021-05-27 16:50:28 +0200

    refact: replacing routes by privateroute

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 import PageNotFound from './pages/page-not-found';
 import PokemonAdd from './pages/pokemon-add';
 import Login from './pages/login';
+import PrivateRoute from './PrivateRoute';
 
 const App: FunctionComponent = () => {
 
@@ -18,12 +19,12 @@ const App: FunctionComponent = () => {
                     </div>
                 </nav>
                 <Switch>
-                    <Route exact path='/' component={PokemonList} />
+                    <PrivateRoute exact path='/' component={PokemonList} />
                     <Route exact path='/login' component={Login} />
-                    <Route exact path='/pokemons' component={PokemonList} />
-                    <Route exact path='/pokemons/add' component={PokemonAdd} />
-                    <Route exact path='/pokemons/edit/:id' component={PokemonEdit} />
-                    <Route path='/pokemons/:id' component={PokemonsDetail} />
+                    <PrivateRoute exact path='/pokemons' component={PokemonList} />
+                    <PrivateRoute exact path='/pokemons/add' component={PokemonAdd} />
+                    <PrivateRoute exact path='/pokemons/edit/:id' component={PokemonEdit} />
+                    <PrivateRoute path='/pokemons/:id' component={PokemonsDetail} />
                     <Route component={PageNotFound} />
                 </Switch>
             </div>
